0d297448e600181661c66e8ca96d4f8b0607f3e9
JoseNugo <37306585+JoseNugo@users.noreply.github.com>
2018-07-30 18:54:19 -0300

    first commit

--- com.nugo.WebBrowser/MainPage.xaml.cs
+++ com.nugo.WebBrowser/MainPage.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +12,50 @@ namespace com.nugo.WebBrowser
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        DispatcherTimer dispatcherTimer;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(1, 0, 0);
+            dispatcherTimer.Start();
+
+
+            webView.LoadCompleted += WebBrowser_LoadCompleted;
+            webView.Loading += WebBrowser_Loading;
+            webView.NavigationStarting += WebView_NavigationStarting;
+
+            InitUrl();
+        }
+
+        private async void InitUrl()
+        {
+            Windows.Storage.StorageFile sampleFile = await Windows.Storage.KnownFolders.DocumentsLibrary.GetFileAsync("url.txt");
+            string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+            webView.Navigate(new Uri(text));
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+            webView.Refresh();
+        }
+
+        private void WebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            _progressBar.Visibility = Visibility.Visible;
+        }
+
+        private void WebBrowser_Loading(FrameworkElement sender, object args)
+        {
+            _progressBar.Visibility = Visibility.Visible;
+        }
+
+        private void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            _progressBar.Visibility = Visibility.Collapsed;
         }
     }
 }
